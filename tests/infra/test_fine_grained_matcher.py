134df6b72ffa9d930aad91e713b8ca24d4ce370e
Andre Sionek <andresionek91@gmail.com>
2024-04-02 23:40:33 +0100

    fix: adjust matcher test

--- tests/infra/test_fine_grained_matcher.py
+++ tests/infra/test_fine_grained_matcher.py
@@ -2,21 +2,25 @@ from aws_cdk.assertions import Template
 from aws_cdk.assertions import Match
 
 
-def test_bucket_name(template: Template):
+def test_bucket_lifecycle_config(template: Template):
     template.has_resource_properties(
         type="AWS::S3::Bucket",
         props={
             "LifecycleConfiguration": {
                 "Rules": [
                     {
-                        "AbortIncompleteMultipartUpload": {"DaysAfterInitiation": Match.any_value()},
-                        "ExpirationInDays": Match.any_value(),
-                        "NoncurrentVersionExpiration": {"NoncurrentDays": Match.any_value()},
+                        "AbortIncompleteMultipartUpload": {
+                            "DaysAfterInitiation": 7
+                        },
+                        "ExpirationInDays": 7,
+                        "NoncurrentVersionExpiration": {
+                            "NoncurrentDays": Match.any_value()
+                        },
                         "Status": "Enabled",
                         "Transitions": [
                             {
                                 "StorageClass": "INTELLIGENT_TIERING",
-                                "TransitionInDays": Match.any_value(),
+                                "TransitionInDays": 60,
                             }
                         ],
                     }
